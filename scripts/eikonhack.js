eb684a7ae8373b500b97c186e763e05121499a7e
Habib Alamin <ha.alamin@gmail.com>
2014-11-22 18:27:49 +0000

    Get basic chart working.

---
+++ scripts/eikonhack.js
@@ -0,0 +1,27 @@
+d3.csv('data/companylist.csv', function(data) {
+  data.sort(function(a, b) {
+    var aValue = parseInt(a.MarketCapInMillions);
+    var bValue = parseInt(b.MarketCapInMillions);
+    if (aValue < bValue)
+      return -1;
+    if (aValue > bValue)
+      return 1;
+    return 0;
+  });
+  console.log(data);
+  var chart = d3.select('.chart');
+  var marketCaps = $.map(data, function(company) { return parseFloat(company.MarketCapInMillions); })
+  var scale = d3.scale.linear()
+                .domain([d3.min(marketCaps), d3.max(marketCaps)])
+                .range([5, parseInt(chart.style('width'))]);
+  chart
+    .selectAll('div')
+      .data(data)
+    .enter().append('div')
+    .style('width', function(d) { return scale(d.MarketCapInMillions) + 'px'; })
+    .style('height', '20px')
+    .style('margin', '2px 0')
+    .style('background', 'red')
+    .style('white-space', 'nowrap')
+    .text(function(d) { return d.Company; });
+});
